7c59cc3bff258e294e8d0903d82bcc64c474673f
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-06-14 22:44:57 +0900

    add:メール認証機能を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { signOut } from "./login/actions";
 
 export default function Home() {
   return (
     <>
+      <form>
+        <button formAction={signOut}>Log out</button>
+      </form>
       <Button>テストです</Button>
       <h1 data-testid="title">Home</h1>
     </>
