868eb81faef79e20e86345fecfde69fbe8b2be16
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-06-15 21:34:15 +0900

    add:検索ページの作成

---
+++ src/app/search/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+// import Link from "next/link";
+import { useState } from "react";
+
+export default function Search() {
+  const [id, setId] = useState("");
+
+  // デモデータ
+  // 本来はAPIから取得することを想定
+  // ここでは簡単なデモデータを使用
+  const demoData = [
+    {
+      id: 1,
+      group: "LE_SSERAFIM",
+      song: "UNFORGIVEN",
+      link: "https://www.youtube.com/shorts/GqbK3rcQkg8?feature=share",
+    },
+    {
+      id: 2,
+      group: "aespa",
+      song: "Whiplash",
+      link: "https://www.youtube.com/shorts/GqbK3rcQkg8?feature=share",
+    },
+    {
+      id: 3,
+      group: "LE_SSERAFIM",
+      song: "ANTIFRAGILE",
+      link: "https://www.youtube.com/shorts/GqbK3rcQkg8?feature=share",
+    },
+    {
+      id: 4,
+      group: "LE_SSERAFIM",
+      song: "UNFORGIVEN",
+      link: "https://www.youtube.com/shorts/GqbK3rcQkg8?feature=share",
+    },
+    {
+      id: 5,
+      group: "aespa",
+      song: "supernova",
+      link: "https://www.youtube.com/shorts/GqbK3rcQkg8?feature=share",
+    },
+  ];
+
+  // デモデータからグループと楽曲のユニークな値を取得
+  const uniqueGroups = demoData.filter(
+    (item, index, self) => index === self.findIndex((v) => v.group === item.group)
+  );
+
+  const uniqueSong = demoData.filter(
+    (item, index, self) => index === self.findIndex((v) => v.song === item.song)
+  );
+
+  return (
+    <>
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold mb-3">楽曲で検索</h2>
+        <div className="flex flex-wrap gap-2">
+          {uniqueSong &&
+            uniqueSong.map((item) => (
+              <Button key={item.id} id="item.id" onClick={() => setId(item.song)}>
+                #{item.song}
+              </Button>
+            ))}
+        </div>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold mb-3">グループで検索</h2>
+        <div className="flex flex-wrap gap-2">
+          {uniqueGroups &&
+            uniqueGroups.map((item) => (
+              <Button key={item.id} id="item.id" onClick={() => setId(item.group)}>
+                #{item.group}
+              </Button>
+            ))}
+        </div>
+      </div>
+
+      <div>
+        <h2 className="text-2xl font-bold mb-3">結果</h2>
+        {demoData &&
+          demoData
+            .filter((item) => item.song === id || item.group === id)
+            .map((item) => (
+              <div key={item.id} className="mb-4">
+                <div>{item.song}</div>
+              </div>
+            ))}
+      </div>
+    </>
+  );
+}
