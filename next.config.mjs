16d4ea37b4fe1e764a874070f1eccb9150dfb04b
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-06-19 23:48:54 +0900

    add:yotubeの画像を読み込むように修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig = {
-  /* config options here */
+  images: {
+    domains: ["i.ytimg.com"],
+  },
 };
 
 if (process.env.NODE_ENV === "development") {
