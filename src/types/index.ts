89b6b87ab284a87b68bb8902c6add0a4d8df97f4
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-06-28 22:27:35 +0900

    update:Recordの型定義の追加

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,12 @@
-export type videos = {
+export type Videos = {
   id: string;
   youtube_id: string;
   title: string;
   thumbnail_url: string;
   view_count: number;
+};
+
+export type Record = {
+  id: string;
+  name: string;
 };
