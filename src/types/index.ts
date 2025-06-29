ebd5a625119f370ac1289f47a5e922f755499094
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-06-29 13:14:54 +0900

    update:型定義の修正

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,18 @@ export type Videos = {
   title: string;
   thumbnail_url: string;
   view_count: number;
+  video_groups: {
+    groups: {
+      id: string;
+      group_name: string;
+    };
+  }[];
+  video_songs: {
+    songs: {
+      id: string;
+      song_name: string;
+    };
+  }[];
 };
 
 export type Record = {
